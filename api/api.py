2cbfb18d28c61e0adb4e441fa615879cb90299f9
Thomas Ung <tomkimsour@gmail.com>
2021-12-03 01:40:08 +0100

    api upload files

--- api/api.py
+++ api/api.py
@@ -1,20 +1,32 @@
-from flask import Flask
-from flask.wrappers import Request
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_restful import reqparse
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+# max file upload value set to 500 mB
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 500
+# allowed extensions
+app.config['UPLOAD_EXTENSIONS'] = ['.mp4', '.mkv', '.mov','.webm','.wmv','.avi','.avchd']
+# path to the writting folder
+app.config['UPLOAD_PATH'] = 'output'
 api = Api(app)
-
 
 # video_send_args = reqparse.RequestParser()
 # video_send_args.add_argument('video')
 
 class VideoConverter(Resource):
     def post(self):
-        print(Request.files)
-        # args = video_send_args.parse_args()
-        # print(args)
+        for uploaded_file in request.files.getlist('file'):
+            if uploaded_file.filename != '':
+                # sanitize file name
+                filename = secure_filename(uploaded_file.filename)
+                file_ext = os.path.splitext(filename)[1]
+                if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                    return 400
+                print(uploaded_file.filename)
+                uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
         return {'task':'File sent'},201
 
 api.add_resource(VideoConverter, '/upload')
